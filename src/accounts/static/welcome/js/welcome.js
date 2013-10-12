42e031dffea8099ca8b7a3e09b402b6be1fc9838
Karthikeyan K <karthipec@gmail.com>
2013-10-12 15:00:43 +0530

    adding welcome page

---
+++ src/accounts/static/welcome/js/welcome.js
@@ -0,0 +1,9 @@
+﻿function HomeController($scope, $http) {
+    $scope.master = {};
+
+    $http.get('http://localhost/F5SeedProject/App/data/users.json')
+        .success(function (data) {
+            $scope.user = {};
+            angular.copy(data.user, $scope.user);
+    });
+}
