b148f864c9b0eb60976dd60e3d59ea6dd0d0f4e2
roddiedude <roddiedude@gmail.com>
2013-10-12 13:33:04 +0530

    updated urls, added views to add/modify user info

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -6,4 +6,8 @@ urlpatterns = patterns('',
      url(r'^signup$', views.signup, name='signup'),
      url(r'^signedup$', views.signedup, name='signuped'),
      url(r'^landing', views.landing, name='landing'),
+     url(r'^login$', views.login, name='login'),
+     url(r'^loggedin$', views.loggedin, name='loggedin'),
+     url(r'^logout', views.logout, name='logout'),
+     url(r'^info', views.info, name='info'),
 )
