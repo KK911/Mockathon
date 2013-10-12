ab993edb1afa882967fb783b48a74fe6338ae954
Karthikeyan K <karthipec@gmail.com>
2013-10-12 14:19:12 +0530

    sign in and sign up pages

--- src/accounts/static/accounts/js/LogonController.js
+++ src/accounts/static/accounts/js/LogonController.js
@@ -1,7 +1,11 @@
-﻿var app = angular.module("signUp", []);
+﻿var app = angular.module("account", ['ui.bootstrap']);
 
 app.directive('disabler', function ($compile) {
     return {
+        scope: {
+            ngModel: '=',
+            form: '='
+        },
         link: function (scope, elm, attrs) {
             var btnContents = $compile(elm.contents())(scope);
             scope.$watch(attrs.ngModel, function (value) {
@@ -17,9 +21,34 @@ app.directive('disabler', function ($compile) {
     }
 })
 
-function SignUpController($scope, $http, $timer) {
+function SignInController($scope, $http, $modal) {
+    $scope.login = function () {
+        $http(
+        {
+            url: 'http://localhost/F5SeedProject/App/data/signUpSuccess.json',
+            method: 'POST',
+            data: $scope.user,
+            headers: { 'Content-Type': 'application/json' }
+        }).success(function (data) {
+            if (data.message == 'success')
+                window.location.href = data.redirect;
+            else {
+                $scope.messageText = data.message;
+                $scope.showMessage();
+                $scope.loading = false;
+            }
+        }).error(function (data) {
+            $scope.messageText = data.message ? data.message : "Error message from the server.";
+            $scope.showMessage();
+            $scope.loading = false;
+        });
+    };
+}
+
+function SignUpController($scope, $http, $modal) {
 
     $scope.isSaveDisabled = function () {
+        console.log($scope.signUpForm.$invalid);
         return $scope.signUpForm.$invalid;
     };
 
@@ -27,16 +56,47 @@ function SignUpController($scope, $http, $timer) {
         $scope.loading = true;
         $http(
         {
-            url: '../data/data.ashx',
+            url: 'http://localhost/F5SeedProject/App/data/signUpSuccess.json',
             method: 'POST',
             data: $scope.user,
             headers: { 'Content-Type': 'application/json' }
         }).success(function (data) {
+            if (data.message == 'success')
+                window.location.href = data.redirect;
+            else {
+                $scope.messageText = data.message;
+                $scope.showMessage();
+                $scope.loading = false;
+            }
+        }).error(function (data) {
+            $scope.messageText = data.message ? data.message : "Error message from the server.";
+            $scope.showMessage();
             $scope.loading = false;
-            $scope.userCreationSuccess = true;
-            $scope.user.id = data.Id;
-            
         });
+
+        $scope.showMessage = function () {
+            var modalInstance = $modal.open({
+                templateUrl: 'message.html',
+                controller: MessageCtrl,
+                resolve: {
+                    messageText: function () {
+                        return $scope.messageText;
+                    }
+                }
+            });
+        };
     };
 };
 
+var MessageCtrl = function ($scope, $modalInstance, messageText) {
+
+    $scope.messageText = messageText;
+
+    $scope.ok = function () {
+        $modalInstance.close();
+    };
+
+    $scope.cancel = function () {
+        $modalInstance.dismiss('cancel');
+    };
+};
