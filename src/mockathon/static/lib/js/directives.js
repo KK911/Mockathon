ec3c6cffb95cb8802ed7e09e9811950f81e125d3
Karthikeyan K <karthipec@gmail.com>
2013-10-14 13:58:19 +0530

    Added services, directives, app js files

--- src/mockathon/static/lib/js/directives.js
+++ src/mockathon/static/lib/js/directives.js
@@ -1,1 +1,20 @@
-﻿
+﻿babybooApp.directive('disabler', function ($compile) {
+    return {
+        scope: {
+            ngModel: '=',
+            form: '='
+        },
+        link: function (scope, elm, attrs) {
+            var btnContents = $compile(elm.contents())(scope);
+            scope.$watch(attrs.ngModel, function (value) {
+                if (value) {
+                    elm.html(scope.$eval(attrs.disabler));
+                    elm.attr('disabled', true);
+                } else {
+                    elm.html('').append(btnContents);
+                    elm.attr('disabled', false);
+                }
+            });
+        }
+    }
+});
